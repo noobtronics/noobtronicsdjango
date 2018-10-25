5d355fa4addde91fe5402fe40eb8386166580ff6
Nikhil Raut <nikhil.raut94@gmail.com>
2018-10-25 09:05:02 +0530

    save

--- mysite/products/urls.py
+++ mysite/products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import *
+from .admin_views import *
 
 
 urlpatterns = [
     path('storeadmin', show_storeadmin),
     path('adminapi/add_product', admin_add_product)
+    path('adminapi/fetch/product', admin_fetch_product)
 ]
