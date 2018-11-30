f68eb554236b4104a75b1928e42339e3badefa98
Nikhil Raut <nikhil.raut94@gmail.com>
2018-11-30 19:01:53 +0530

    save

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -172,9 +172,9 @@ DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760
 PAYTM = {
     "MID": "LUMINO65914372097499",
     "CHANNEL_ID": "WEB",
-    "WEBSITE": "WEBSTAGING",
+    "WEBSITE": "WEBPROD",
     "INDUSTRY_TYPE_ID": "Retail",
-    "MERCHANT_KEY": "9bz1jMoShd5bLiQC",
+    "MERCHANT_KEY": "dKFylaPL@kSlyIti",
     "Transaction_URL": "https://securegw.paytm.in/theia/processTransaction",
     "Status_URL": "https://securegw.paytm.in/merchant-status/getTxnStatus",
     "CALLBACK_URL": "http://localhost:8000/cart/paytm/callback"
