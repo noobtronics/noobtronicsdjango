6ccf454102459afa589c87c9d921297f6edcc995
Nikhil Raut <nikhil.raut94@gmail.com>
2018-11-30 16:03:53 +0530

    save

--- mysite/products/admin.py
+++ mysite/products/admin.py
@@ -1,21 +1,49 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'pagetitle', 'price', 'mrp_price', 'quantity_available', 'in_stock', 'is_published')
+admin.site.register(Product, ProductAdmin)
+
 admin.site.register(MainImage)
 admin.site.register(Image)
 admin.site.register(Thumbnail)
 admin.site.register(HomePage)
 
-admin.site.register(UserCode)
-admin.site.register(Cart)
+
+class UserCodeAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'code')
+    search_fields = ('code',)
+admin.site.register(UserCode, UserCodeAdmin)
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'cart_state', 'created')
+    search_fields = ('user_id',)
+admin.site.register(Cart, CartAdmin)
+
+
 admin.site.register(CartObjects)
-admin.site.register(Waitlist)
+
+
+class WaitlistAdmin(admin.ModelAdmin):
+    list_display = ('prod_id', 'user_id', 'created')
+    search_fields = ('prod_id',)
+admin.site.register(Waitlist, WaitlistAdmin)
 
 
 admin.site.register(ProductDetails)
 
 admin.site.register(Tags)
 
+class PaytmHistoryAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'txn_amount', 'txn_date', 'status')
+    search_fields = ('user_id',)
 
-admin.site.register(PaytmHistory)
+admin.site.register(PaytmHistory, PaytmHistoryAdmin)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('order_id', 'user_id', 'order_state')
+    search_fields = ('order_id',)
+admin.site.register(Orders, OrderAdmin)
