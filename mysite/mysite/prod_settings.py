87994c22b1f8229cbc2b7ce89054f3468ef937fb
Nikhil Raut <nikhil.raut94@gmail.com>
2019-01-22 19:20:57 +0530

    footer height fix

--- mysite/mysite/prod_settings.py
+++ mysite/mysite/prod_settings.py
@@ -4,3 +4,14 @@ DEBUG = False
 
 
 PAYTM["CALLBACK_URL"] = "https://noobtronics.ltd/cart/paytm/callback"
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'noobdb',
+        'USER': 'noobuser',
+        'PASSWORD': '123123',
+        'HOST': 'localhost'
+    }
+}
