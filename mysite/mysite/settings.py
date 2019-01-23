ed7040d22cde49535a5213be076fa249aab0a5dc
Nikhil Raut <nikhil.raut94@gmail.com>
2019-01-23 21:06:42 +0530

    sa

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ndb1',
+        'NAME': 'noobdb',
         'USER': 'noobuser',
         'PASSWORD': '123123',
         'HOST': 'localhost'
