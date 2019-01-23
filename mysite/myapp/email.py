39b2e5880e2a13befee270f18621fc548fcfe057
Nikhil Raut <nikhil.raut94@gmail.com>
2019-01-23 12:34:06 +0530

    save

--- mysite/myapp/email.py
+++ mysite/myapp/email.py
@@ -4,6 +4,7 @@ import pytz
 from django.template import Context, Template
 from django.template.loader import get_template
 from django.http import HttpResponse
+from email.mime.image import MIMEImage
 
 
 IST_TZ = pytz.timezone('Asia/Kolkata')
@@ -12,44 +13,78 @@ IST_TZ = pytz.timezone('Asia/Kolkata')
 def test_mail(request):
     ordr = Orders.objects.all()[0]
 
-    rendered_data = get_order_confirm_data(ordr)
+    rendered_data, images_data = get_order_confirm_data(ordr)
 
-    send_mail(
+    message = EmailMultiAlternatives(
         subject='Your noobtronics.ltd order - #{0} has been received'.format(ordr.order_id),
-        html_message = rendered_data,
-        message = '',
-        from_email = 'Noobtronics Shop <no-reply@noobtronics.ltd>',
-        recipient_list = ['nikhil.raut94@gmail.com'],
-        fail_silently=False,
+        body='',
+        from_email='Noobtronics Shop <no-reply@noobtronics.ltd>',
+        to=['nikhil.raut94@gmail.com'],
     )
+
+    message.content_subtype = "html"
+    message.attach_alternative(rendered_data, "text/html")
+    message.attach(logo_data())
+    for img in images_data:
+        message.attach(get_img_data(img['path'], img['id']))
+
+    message.send(fail_silently=False)
+
+
+
+def logo_data():
+    with open('static/images/logo_email.png', 'rb') as f:
+        logo_data = f.read()
+    logo = MIMEImage(logo_data)
+    logo.add_header('Content-ID', '<logo>')
+    return logo
+
+
+def get_img_data(path, cid):
+    with open(path, 'rb') as f:
+        logo_data = f.read()
+    logo = MIMEImage(logo_data)
+    logo.add_header('Content-ID', '<{0}>'.format(cid))
+    return logo
+
 
 
 def get_order_confirm_data(ordr):
     ordr = Orders.objects.all()[0]
 
+    order_prods = OrderProducts.objects.filter(order_id=ordr)
+
     products = []
-    temp = ['data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAwBAMAAACh2TSJAAAALVBMVEUAAADtNTX////3n5/+9fX719f7zMz5tLTzfHzuQED//f31jY3ybGzxXV3wVFRaxp+rAAAAAXRSTlMAQObYZgAAALVJREFUOMut0rENAjEQRNHdC4kY0QBaAQUQX0QAFSAKIKQEKiAA6VqgIkriApuV1x7pQPz0aWwHljLMpZ0CRDBGoXmeghGYKFJsUo90giAImCgV5OJF+oOgKE48MlGgs2VLBIunWesw0a1ZHqF82c7GmmIfUSpgotOly29DFPFJFDEhkgIT/V5mZuvj6XofKrHU6vyI4u37IYi36aN4h5tL7PJyif1dvCgEpapzISbCTEj5R78BZq5A5Ldh2XYAAAAASUVORK5CYII',
-            'Smartduino UNO SMD', 'Atmega328PB microController Board','1','550', 'link']
-    products.append(temp)
-    products.append(temp)
-    products.append(temp)
+    images_data = []
+
+    for op in order_prods:
+        temp = ['cid:{0}'.format(op.prod_id.id),
+                op.prod_id.name, op.prod_id.pagetitle,op.quantity,op.subtotal, 'https://noobtronics.ltd/product/'+op.prod_id.slug]
+        products.append(temp)
+
+        temp_i = {
+            'id': op.prod_id.id,
+            'path': op.prod_id.mainimage.img_data.th_micro.image.path
+        }
+        images_data.append(temp_i)
 
     data = {
         'order': ordr,
         'order_time': ordr.created.astimezone(IST_TZ).strftime('%d %b %Y %I:%M%p'),
         'sub_total': ordr.total_amount - ordr.delivery_charge - ordr.extra_charge,
-        'products': products
+        'products': products,
+        'images_data': images_data,
     }
 
     template = get_template('email_confirm.html')
     rendered_data = template.render(data)
-    return rendered_data
+    return rendered_data, images_data
 
 
 
 def test_mail_web(request):
     ordr = Orders.objects.all()[0]
 
-    rendered_data = get_order_confirm_data(ordr)
+    rendered_data, images_data = get_order_confirm_data(ordr)
 
     return HttpResponse(rendered_data)
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "collectstatic")
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "")
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
