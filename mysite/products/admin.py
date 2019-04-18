9e6b1a675cc005388c0b787665532f513d857098
Nikhil Raut <nikhil.raut94@gmail.com>
2019-04-18 11:59:49 +0530

    save

--- mysite/products/admin.py
+++ mysite/products/admin.py
@@ -3,6 +3,8 @@ from .models import *
 from django.contrib.auth.models import User, Group
 from django.forms import TextInput, Textarea
 
+admin.site.unregister(User)
+
 
 class ReadOnlyAdmin(admin.ModelAdmin):
     # disables edits
