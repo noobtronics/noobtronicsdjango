fe45f77c72007fac7b16258cb111f7b8f4c49575
Nikhil Raut <nikhil.raut94@gmail.com>
2019-06-23 15:32:31 +0530

    save

--- mysite/mysite/prod_settings.py
+++ mysite/mysite/prod_settings.py
@@ -15,3 +15,11 @@ DATABASES = {
         'HOST': 'localhost'
     }
 }
+
+
+RAZORPAY = {
+    'key_id': 'rzp_live_yke0EGCry8eh0Q',
+    'key_secret': 'RXU23lw9jIvkGgTYh0MDWyUk',
+    'callback_url':'https://noobtronics.ltd/cart/razorpay/callback',
+    'cancel_url':'https://noobtronics.ltd/cart?status=fail',
+}
