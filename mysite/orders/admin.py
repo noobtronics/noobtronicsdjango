415a4e704c32f23d2212b0dc8557b31908e87955
Nikhil R <nikhil.raut94@gmail.com>
2020-04-19 00:50:45 +0530

    Update admin.py

--- mysite/orders/admin.py
+++ mysite/orders/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from products.models import UrlHistory
 
 # Register your models here.
+class UrlHistoryAdmin(admin.ModelAdmin):
+    list_display = ('url', 'mmid', 'csrf_token','referer', 'remote_addr','visited')
+
+
+admin.site.register(UrlHistory, UrlHistoryAdmin)
