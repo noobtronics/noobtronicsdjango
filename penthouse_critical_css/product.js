da7c647449ae740c8a91694a13fb8577bf98989a
Nikhil R <nikhil.raut94@gmail.com>
2020-06-20 16:36:27 +0530

    s

---
+++ penthouse_critical_css/product.js
@@ -0,0 +1,21 @@
+const penthouse = require('penthouse')
+const fs = require('fs')
+
+penthouse({
+  url: 'http://localhost:8000/product/arduino-uno',
+  css: '/Users/Nikhil/Desktop/legacy/noobtronicsdjango/mysite/static/css/bulma/mystyle.min.css',
+  width: 1366,
+  height: 768,
+  renderWaitTime: 5000,
+  keepLargerMediaQueries: true,
+  blockJSRequests: false,
+  screenshots: {
+    basePath: 'productpage', // absolute or relative; excluding file extension
+    type: 'jpeg', // jpeg or png, png default
+    quality: 80 // only applies for jpeg type
+  }
+})
+.then(criticalCss => {
+  // use the critical css
+  fs.writeFileSync('productpage.min.css', criticalCss);
+})
